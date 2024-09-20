b311ef1a0e9d7b8390a2779a3248680c7fc3566e
ragecodemode <nikitaloskutov18@gmail.com>
2024-09-20 20:55:54 +0300

    handlers

--- core/api/v1/tasks/handlers/tasks.py
+++ core/api/v1/tasks/handlers/tasks.py
@@ -1,9 +1,4 @@
-# from django.shortcuts import get_object_or_404
 from rest_framework import viewsets
-# from rest_framework.decorators import action
-# from rest_framework.permissions import AllowAny
-# from rest_framework.response import Response
-# from rest_framework import status
 
 from core.api.v1.tasks.models.tasks import Task
 from core.api.v1.tasks.shemas.serializers import TaskSerializer
